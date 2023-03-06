f863fffef29de6000f31d53d20766fc589581cb5
ABOUTABIT <htabit999@gmail.com>
2023-03-06 20:31:56 +0100

    Tache Projet 2 Commit

--- app/src/main/java/com/example/fbapplication/ProjetActivity.kt
+++ app/src/main/java/com/example/fbapplication/ProjetActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import androidx.lifecycle.viewmodel.CreationExtras
 import com.google.firebase.database.FirebaseDatabase
 import java.util.*
 
@@ -25,16 +26,16 @@ class ProjetActivity : AppCompatActivity() {
         var datefp : String = findViewById<EditText>(R.id.editTextPDateF).text.toString()
         var descr1p : String = findViewById<EditText>(R.id.descriptionp1_edit_text).text.toString()
         var descr2p : String = findViewById<EditText>(R.id.descriptionp2_edit_text).text.toString()
-        if (namep != null || chef != null || statusp != null || datedp != null || datefp != null || descr1p != null || descr2p != null  )
+        if (namep.isEmpty() || chef.isEmpty() || statusp.isEmpty() || datedp.isEmpty() || datefp.isEmpty() || descr1p.isEmpty() || descr2p.isEmpty()  )
+        {
+            Toast.makeText(this, "IL faut remplir tous les champs", Toast.LENGTH_SHORT).show()
+        }
+        else
         {
             val projet = Projet(namep, descr1p, descr2p, chef, datedp, datefp, statusp)
             db.child("Base").child("Projet").child(UUID.randomUUID().toString()).push()
                 .setValue(projet)
             startActivity(Intent(this, MenuActivity::class.java))
-        }
-        else
-        {
-            Toast.makeText(this, "IL faut remplir tous les champs", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/example/fbapplication/TacheActivity.kt
+++ app/src/main/java/com/example/fbapplication/TacheActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.FirebaseDatabase
 import java.util.*
@@ -18,15 +19,21 @@ class TacheActivity : AppCompatActivity() {
 
     public fun addTache(view: View) {
         var name: String = findViewById<EditText>(R.id.nom_edit_text).text.toString()
-        var collab : String = findViewById<EditText>(R.id.collaborateur_edit_text).text.toString()
-        var status : String = findViewById<EditText>(R.id.status_edit_text).text.toString()
-        var dated : String = findViewById<EditText>(R.id.editTextDateD).text.toString()
-        var datef : String = findViewById<EditText>(R.id.editTextDateF).text.toString()
-        var descr1 : String = findViewById<EditText>(R.id.description1_edit_text).text.toString()
-        var descr2 : String = findViewById<EditText>(R.id.description2_edit_text).text.toString()
-
-        val tache = Tache(name,descr1,descr2,collab,dated,datef,status)
-        db.child("Base").child("Tache").child(UUID.randomUUID().toString()).push().setValue(tache)
-        startActivity(Intent(this, MenuActivity::class.java))
+        var collab: String = findViewById<EditText>(R.id.collaborateur_edit_text).text.toString()
+        var status: String = findViewById<EditText>(R.id.status_edit_text).text.toString()
+        var dated: String = findViewById<EditText>(R.id.editTextDateD).text.toString()
+        var datef: String = findViewById<EditText>(R.id.editTextDateF).text.toString()
+        var descr1: String = findViewById<EditText>(R.id.description1_edit_text).text.toString()
+        var descr2: String = findViewById<EditText>(R.id.description2_edit_text).text.toString()
+        if (name.isEmpty() || collab.isEmpty() || status.isEmpty() || dated.isEmpty() || datef.isEmpty() || descr1.isEmpty() || descr2.isEmpty() )
+        {
+            Toast.makeText(this, "Il faut remplir tous les champs", Toast.LENGTH_SHORT).show()
         }
+        else
+        {
+            val tache = Tache(name, descr1, descr2, collab, dated, datef, status)
+            db.child("Base").child("Tache").child(UUID.randomUUID().toString()).push().setValue(tache)
+            startActivity(Intent(this, MenuActivity::class.java))
+        }
+    }
     }
