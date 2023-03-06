f7dcc64b37f873d14ae713be52952f4bafdb2cbb
ABOUTABIT <htabit999@gmail.com>
2023-03-06 13:38:16 +0100

    UUID Commit

--- app/src/main/java/com/example/fbapplication/TacheActivity.kt
+++ app/src/main/java/com/example/fbapplication/TacheActivity.kt
@@ -5,13 +5,14 @@ import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.FirebaseDatabase
+import java.util.*
 
 class TacheActivity : AppCompatActivity() {
     var db = FirebaseDatabase.getInstance().getReference("projet-4f405")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tache)
+        setContentView(R.layout.activity_tache).toString()
     }
 
     public fun addTache(view: View) {
@@ -22,7 +23,7 @@ class TacheActivity : AppCompatActivity() {
         var datef : String = findViewById<EditText>(R.id.editTextDateF).text.toString()
         var descr : String = findViewById<EditText>(R.id.description_edit_text).text.toString()
         val tache = Tache(name, descr,collab,dated,datef,status)
-        db.child("IDTache").child("Tache").push()
-        db.child("IDTache").child("Tache").setValue(tache)
+
+        db.child("IDTache").child("Tache").child(UUID.randomUUID().toString()).push().setValue(tache)
         }
     }
